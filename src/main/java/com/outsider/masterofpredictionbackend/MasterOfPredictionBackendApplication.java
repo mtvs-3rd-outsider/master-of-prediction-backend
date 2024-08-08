4d78727dd5a5efc43065901051e18cd5ffa75227
윤여원 <dudnjsckrgo@gmail.com>
2024-08-08 17:38:47 +0900

    first commit

---
+++ src/main/java/com/outsider/masterofpredictionbackend/MasterOfPredictionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.outsider.masterofpredictionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterOfPredictionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterOfPredictionBackendApplication.class, args);
+	}
+
+}
