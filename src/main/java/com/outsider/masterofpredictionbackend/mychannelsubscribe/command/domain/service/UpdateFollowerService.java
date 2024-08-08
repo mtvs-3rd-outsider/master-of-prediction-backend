720eb2726567f776b0e2fe82e73137ef917899aa
윤여원 <dudnjsckrgo@gmail.com>
2024-08-08 22:51:50 +0900

    feat: mychannel 구독 의 외부 서비스 구현체 update 구독수 기능

---
+++ src/main/java/com/outsider/masterofpredictionbackend/mychannelsubscribe/command/domain/service/UpdateFollowerService.java
@@ -0,0 +1,6 @@
+package com.outsider.masterofpredictionbackend.mychannelsubscribe.command.domain.service;
+
+public interface UpdateFollowerService {
+
+    public void updateFollowerMyChannel(Long channelId, boolean isPlus);
+}
