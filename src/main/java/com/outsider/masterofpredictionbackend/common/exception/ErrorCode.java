4d1e5e73b7f0fe321c8cf77614233bdd021499ba
Isaiah IM <46448947+isaiahIM@users.noreply.github.com>
2024-08-12 09:21:30 +0900

    Feat: 공통 컴포넌트 추가 - 표준 예외객체 필드 추가

--- src/main/java/com/outsider/masterofpredictionbackend/common/exception/ErrorCode.java
+++ src/main/java/com/outsider/masterofpredictionbackend/common/exception/ErrorCode.java
@@ -1,4 +1,24 @@
 package com.outsider.masterofpredictionbackend.common.exception;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public enum ErrorCode {
+
+    EXAMPLE_ERROR(HttpStatus.BAD_REQUEST, "예외 예시", "Example exception", "EXAMPLE");
+
+
+    private final String description;
+    private final HttpStatus status;
+    private final String code;
+    private final String errorType;
+
+    ErrorCode(HttpStatus status, String errorType, String description, String code) {
+        this.status = status;
+        this.errorType = errorType;
+        this.description = description;
+        this.code = code;
+    }
+
 }
