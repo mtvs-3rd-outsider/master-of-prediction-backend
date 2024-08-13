82216447f151618c0c9422315c3da0218569a1de
jeonjinhyun <jjh06500@gmail.com>
2024-08-13 17:41:29 +0900

    peed-entity

---
+++ src/main/java/com/outsider/masterofpredictionbackend/peed/command/domain/aggregate/embedded/User.java
@@ -0,0 +1,26 @@
+package com.outsider.masterofpredictionbackend.peed.command.domain.aggregate.embedded;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class User {
+    private Long userId;
+
+    public User() {
+    }
+
+    public User(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                '}';
+    }
+}
