e7986cdd9624c137e16adda6efd303fe091b3d73
jeonjinhyun <jjh06500@gmail.com>
2024-08-26 14:32:26 +0900

    feed aggregate

---
+++ src/main/java/com/outsider/masterofpredictionbackend/feed/command/domain/aggregate/embedded/Guest.java
@@ -0,0 +1,39 @@
+package com.outsider.masterofpredictionbackend.feed.command.domain.aggregate.embedded;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Guest {
+
+    @Column(name = "post_guest_id")
+    private String guestId;
+
+    @Column(name = "post_guest_password")
+    private String guestPassword;
+
+    public Guest() {}
+
+    public Guest(String guestId, String guestPassword) {
+        this.guestId = guestId;
+        this.guestPassword = guestPassword;
+    }
+
+    public String getGuestId() {
+        return guestId;
+    }
+
+    public String getGuestPassword() {
+        return guestPassword;
+    }
+
+    @Override
+    public String toString() {
+        return "Guest{" +
+                "guestId='" + guestId + '\'' +
+                ", guestPassword='" + guestPassword + '\'' +
+                '}';
+    }
+
+}
