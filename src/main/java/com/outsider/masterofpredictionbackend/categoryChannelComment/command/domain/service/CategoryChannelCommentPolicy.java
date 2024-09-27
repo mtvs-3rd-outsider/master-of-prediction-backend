791305b09bdf5580857cac70d7ae9582f74e2b95
Isaiah IM <46448947+isaiahIM@users.noreply.github.com>
2024-09-27 09:23:59 +0900

    #feat/comment_api feat: 카테고리 채널 댓글 로그인 사용자 연결

--- src/main/java/com/outsider/masterofpredictionbackend/categoryChannelComment/command/domain/service/CategoryChannelCommentPolicy.java
+++ src/main/java/com/outsider/masterofpredictionbackend/categoryChannelComment/command/domain/service/CategoryChannelCommentPolicy.java
@@ -3,20 +3,20 @@ package com.outsider.masterofpredictionbackend.categoryChannelComment.command.do
 import com.outsider.masterofpredictionbackend.categoryChannelComment.command.domain.aggregate.CategoryChannelComment;
 import com.outsider.masterofpredictionbackend.categoryChannelComment.command.domain.aggregate.embedded.WriterInfo;
 import com.outsider.masterofpredictionbackend.categoryChannelComment.command.domain.service.dto.LoginUserInfo;
+import com.outsider.masterofpredictionbackend.user.command.application.dto.CustomUserInfoDTO;
 
 import java.util.Optional;
 
 public interface CategoryChannelCommentPolicy {
-    boolean isLogin();
 
-    LoginUserInfo getLoginUserInfo();
+    LoginUserInfo getLoginUserInfo(CustomUserInfoDTO userInfoDTO);
 
-    boolean isMatchUserInfo(CategoryChannelComment comment);
+    boolean isMatchUserInfo(CategoryChannelComment comment, CustomUserInfoDTO userInfoDTO);
 
     boolean isMatchPassword(CategoryChannelComment comment, String password);
 
     WriterInfo generateAnonymousUser();
 
 
-    Optional<CategoryChannelComment> getCommentById(Long id);
+    Optional<CategoryChannelComment> getCommentById(Long id, CustomUserInfoDTO userInfoDTO);
 }
