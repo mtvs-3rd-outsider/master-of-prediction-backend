1903733c4d75977565529e7f62654938b1fe27a4
jeonjinhyun <jjh06500@gmail.com>
2024-10-18 09:30:58 +0900

    myChannel feed view service

--- src/main/java/com/outsider/masterofpredictionbackend/feed/command/application/controller/CategoryChannelFeedController.java
+++ src/main/java/com/outsider/masterofpredictionbackend/feed/command/application/controller/CategoryChannelFeedController.java
@@ -1,4 +1,11 @@
 package com.outsider.masterofpredictionbackend.feed.command.application.controller;
 
+import org.checkerframework.checker.units.qual.K;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/feeds")
 public class CategoryChannelFeedController {
+
 }
--- src/main/java/com/outsider/masterofpredictionbackend/feed/command/application/controller/MyChannelFeedController.java
+++ src/main/java/com/outsider/masterofpredictionbackend/feed/command/application/controller/MyChannelFeedController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/feeds")
+@RequestMapping("/api/v1/myfeeds")
 public class MyChannelFeedController {
     private final MyChannelFeedService myChannelFeedService;
 
