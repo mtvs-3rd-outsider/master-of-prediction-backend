812841109e3d0c7b78b62a2b3fd6b989005db1d3
윤여원 <dudnjsckrgo@gmail.com>
2024-11-16 18:53:23 +0900

    fix: redirect url

--- src/main/java/com/outsider/masterofpredictionbackend/config/SecurityConfig.java
+++ src/main/java/com/outsider/masterofpredictionbackend/config/SecurityConfig.java
@@ -165,7 +165,7 @@ public class SecurityConfig {
                 .clientSecret(clientSecret)
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .redirectUri("https://master-of-prediction.shop:8081/login/oauth2/code/google")
+                .redirectUri(redirectUri)
                 .scope("profile", "email")
                 .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
                 .tokenUri("https://www.googleapis.com/oauth2/v4/token")
