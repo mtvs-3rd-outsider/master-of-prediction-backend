ad9b5300a28aa7c17c1e040f22e57eb81807f7ee
jeonjinhyun <jjh06500@gmail.com>
2024-11-20 18:20:02 +0900

    feed fix

--- src/main/java/com/outsider/masterofpredictionbackend/config/SecurityConfig.java
+++ src/main/java/com/outsider/masterofpredictionbackend/config/SecurityConfig.java
@@ -88,7 +88,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         // .requestMatchers("/**").permitAll()
-                        .requestMatchers("/api/v1/auth/login","/api/v1/auth/register","/api/v1/auth/signup/email","/api/v1/auth/signup/emailAuth","/api/v1/auth/forgot-password","/api/v1/auth/reset-password","/api/v1/feeds","/api/v1/feeds/**/verify-guest").permitAll()
+                        .requestMatchers("/api/v1/auth/login","/api/v1/auth/register","/api/v1/auth/signup/email","/api/v1/auth/signup/emailAuth","/api/v1/auth/forgot-password","/api/v1/auth/reset-password","/api/v1/feeds","/api/v1/feeds/{feedId}/**").permitAll()
                         .requestMatchers("/**").access(customAuthorizationManager)
                         .anyRequest().authenticated()
                 )
